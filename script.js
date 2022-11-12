01c14269db67c0d05c3d5614a99552e078d1e423
Glitch (glitch-hello-website) <none>
2022-11-12 20:01:53 +0000

    🚃🎇 Checkpoint ./script.js:46161017/830:46161952/426:50979550/442 ./Questions/question1.html:46161952/17

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 let questionNum = 0;
 
+let qCount = 1;
+
 function nextQuestion() {
   if(questionNum = 1) {
     $(".question").text("Do you prefer day or night?");
     $("#indoors").attr("id", "day");
     $("#outdoors").attr("id", "night");
+    qCount++;
+    $("#qNum").text(qCount);
   }
     if(questionNum = 2) {
-    $(".question").text("Do you prefer day or night?");
-    $("#indoors").attr("id", "day");
-    $("#outdoors").attr("id", "night");
+    $(".question").text("Do you prefer the sea or mountain?");
+    $("#indoors").attr("id", "sea");
+    $("#outdoors").attr("id", "mountain");
   }
+      if(questionNum = 3) {
+    $(".question").text("Do you prefer libraries or cafes?");
+    $("#indoors").attr("id", "sea");
+    $("#outdoors").attr("id", "mountain");
+  }
+      if(questionNum = 4) {
+    $(".question").text("Do you prefer the sound of rain or fire?");
+    $("#indoors").attr("id", "sea");
+    $("#outdoors").attr("id", "mountain");
+  }
+  
 }
 
 $("#indoors").click(function() {
@@ -23,4 +38,22 @@ $("#outdoors").click(function() {
   nextQuestion();
 });
 
+$("#day").click(function() {
+  questionNum = 3;
+  nextQuestion();
+});
 
+$("#night").click(function() {
+  questionNum = 4;
+  nextQuestion();
+});
+
+$("#sea").click(function() {
+  questionNum = 5;
+  nextQuestion();
+});
+
+$("#mountains").click(function() {
+  questionNum = 6;
+  nextQuestion();
+});
